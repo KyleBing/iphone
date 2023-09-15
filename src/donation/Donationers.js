74f661644ff2325eab687a016c4506507f493ea3
KyleBing <kylebing@163.com>
2023-09-15 17:01:32 +0800

    update Donationers.js

--- src/donation/Donationers.js
+++ src/donation/Donationers.js
@@ -5,7 +5,7 @@
  *  @amount: 金额
  */
 export default [
-    {name: '留天', amount: 10},
-    {name: '*脚', amount: 10},
-    {name: '*鱼', amount: 0.5},
+    {date: '2023-09-15', name: '留天', amount: 10},
+    {date: '2023-09-15', name: '*鱼', amount: 0.5},
+    {date: '2023-09-13', name: '*脚', amount: 10},
 ]
