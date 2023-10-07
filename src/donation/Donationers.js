16f062b239a21ff7f1b6905eb2db156e8950881f
KyleBing <kylebing@163.com>
2023-10-07 17:19:43 +0800

    update Donationers.js

--- src/donation/Donationers.js
+++ src/donation/Donationers.js
@@ -7,6 +7,7 @@
  */
 export default [
     {date: '2023-10-03', method: '微信', name: '刘~', amount: 5},
+    {date: '2023-10-01', method: '支付宝', name: '银色灰尘', amount: 8.8},
     {date: '2023-09-28', method: '微信', name: '贝尔捡', amount: 5},
     {date: '2023-09-15', method: '微信', name: '留天', amount: 10},
     {date: '2023-09-15', method: '微信', name: '*鱼', amount: 0.5},
