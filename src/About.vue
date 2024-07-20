36daf9d26a690c5b8397c1fdfb1d7dd9709f98d3
KyleBing(MBP14) <kylebing@163.com>
2024-07-20 15:16:13 +0800

    wss -> ws

--- src/About.vue
+++ src/About.vue
@@ -86,7 +86,7 @@ export  default {
                 })
         },
         websocketInit() {
-            this.websocket = new WebSocket('wss://kylebing.cn/ws')
+            this.websocket = new WebSocket('ws://kylebing.cn/ws')
             this.websocket.onopen = this.websocketOnOpen
             this.websocket.onmessage = this.websocketOnMessage
             this.websocket.onerror = this.websocketOnError
